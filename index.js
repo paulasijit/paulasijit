d80dfe0e02c19e831d395962b48a4d1adc5aeb56
Asijit Paul <asijit1610@gmail.com>
2021-01-15 21:00:15 +0530

    COMMIT

--- index.js
+++ index.js
@@ -59,9 +59,10 @@ const readme = `\
 ## &#x1f4c8; GitHub Stats
 ![Asijit's github stats](https://github-readme-stats.vercel.app/api?username=paulasijit&show_icons=true&title_color=ffc857&icon_color=8ac926&text_color=daf7dc&bg_color=151515)
 [![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=paulasijit&layout=compact&text_color=daf7dc&bg_color=151515)](https://github.com/anuraghazra/github-readme-stats)
+## CODING STATS
 <!--START_SECTION:waka-->
-
 <!--END_SECTION:waka-->
+## YEAR PROGRESS BAR
 ⏳ **Year Progress** ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} % as on ⏰ ${new Date().getDate()+'-'+(new Date().getMonth()+1)+'-'+new Date().getFullYear()+'.'}
 
 
