0ae2cb0b2161eb6e79a17080b9a11ccc0eeaca7c
Asijit Paul <34892787+paulasijit@users.noreply.github.com>
2024-05-23 10:59:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ const readme = `\
 ### Hi there 👋
 
 [![Header](https://raw.githubusercontent.com/paulasijit/paulasijit/main/asijit_gif.gif)](https://www.linkedin.com/in/asijit-paul-2142881a2/)
-<!-- If you want the template for my gif, email me! -->
 ![Follow](https://img.shields.io/github/followers/paulasijit?label=Follow&style=social)
 
 ### Languages
@@ -29,22 +28,23 @@ const readme = `\
 [![C](https://img.shields.io/badge/-C-000?&logo=C)](https://github.com/paulasijit?tab=repositories&q=&type=&language=c)
 [![Java](https://img.shields.io/badge/-Java-000?&logo=Java&logoColor=007396)](https://github.com/paulasijit?tab=repositories&q=&type=&language=java)
 [![TypeScript](https://img.shields.io/badge/-TypeScript-000?&logo=TypeScript&logoColor=007ACC)](https://github.com/paulasijit?tab=repositories&q=&type=&language=typescript)
-[![C++](https://img.shields.io/badge/-C++-000?&logo=c%2b%2b&logoColor=00599C)](https://github.com/paulasijit?tab=repositories&q=&type=&language=c++)
+[![C++](https://img.shields.io/badge/-C++-000?&logo=c%2b%2b&logoColor=00599C)](https://github.com/paulasijit?tab=repositories&q=&type=&language=cpp)
+[![Go](https://img.shields.io/badge/-Golang-000?&logo=go)](https://github.com/paulasijit?tab=repositories&q=&type=&language=go)
 [![SQL](https://img.shields.io/badge/-SQL-000?&logo=MySQL&logoColor=4479A1)](https://github.com/paulasijit?tab=repositories&q=&type=&language=sql)
 [![Swift](https://img.shields.io/badge/-Swift-000?&logo=Swift)](https://github.com/paulasijit?tab=repositories&q=&type=&language=swift)
+[![Node.js](https://img.shields.io/badge/-Node.js-000?&logo=node.js)](https://github.com/paulasijit?tab=repositories&q=&type=&language=node)
+![React](https://img.shields.io/badge/-React-000?&logo=React)
 
 ### Technologies
 
-<!-- ![AWS](https://img.shields.io/badge/-AWS-000?&logo=Amazon-AWS&logoColor=FF9900) -->
-<!-- ![CI/CD](https://img.shields.io/badge/-CI%2FCD-000?&logo=CircleCI&logoColor=888) -->
-<!-- ![Docker](https://img.shields.io/badge/-Docker-000?&logo=Docker) -->
-<!-- ![Jira](https://img.shields.io/badge/-Jira-000?&logo=Jira-Software&logoColor=0052CC) -->
-<!-- ![Kubernetes](https://img.shields.io/badge/-Kubernetes-000?&logo=Kubernetes) -->
+![GIT](https://img.shields.io/badge/-Git-000?&logo=Git)
+![AWS](https://img.shields.io/badge/-AWS-000?&logo=Amazon-AWS&logoColor=FF9900)
+![CI/CD](https://img.shields.io/badge/-CI%2FCD-000?&logo=CircleCI&logoColor=888)
+![Docker](https://img.shields.io/badge/-Docker-000?&logo=Docker)
+![Jira](https://img.shields.io/badge/-Jira-000?&logo=Jira-Software&logoColor=0052CC)
+![Kubernetes](https://img.shields.io/badge/-Kubernetes-000?&logo=Kubernetes)
 ![Linux](https://img.shields.io/badge/-Linux-000?&logo=Linux&logoColor=FCC624)
-![Node.js](https://img.shields.io/badge/-Node.js-000?&logo=node.js)
-![React](https://img.shields.io/badge/-React-000?&logo=React)
-<!-- ![Spring](https://img.shields.io/badge/-Spring-000?&logo=Spring) -->
-<!-- ![TCP/IP](https://img.shields.io/badge/-TCP%2FIP-000?&logo=Cisco) -->
+![Jenkins](https://img.shields.io/badge/-Jenkins-000?&logo=Jenkins)
 
 ### Full Stack Projects
 
@@ -59,9 +59,11 @@ const readme = `\
 ## &#x1f4c8; GitHub Stats
 ![Asijit's github stats](https://github-readme-stats.vercel.app/api?username=paulasijit&show_icons=true&title_color=ffc857&icon_color=8ac926&text_color=daf7dc&bg_color=151515)
 [![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=paulasijit&layout=compact&text_color=daf7dc&bg_color=151515)](https://github.com/anuraghazra/github-readme-stats)
+
 ## CODING STATS
-<!--START_SECTION:waka-->
-<!--END_SECTION:waka-->
+<!-- START_SECTION:waka
+END_SECTION:waka -->
+
 ## YEAR PROGRESS BAR
 ⏳ **Year Progress** ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} % as on ⏰ ${new Date().getDate()+'-'+(new Date().getMonth()+1)+'-'+new Date().getFullYear()+'.'}
 
@@ -74,12 +76,16 @@ const readme = `\
 
 ## 🗂️ Highlight Projects
 
+<a href="https://github.com/paulasijit/terminal-site">
+  <img align="center" src="https://github-readme-stats.vercel.app/api/pin/?username=paulasijit&repo=terminal-site&show_icons=true&line_height=27&title_color=6aa6f8&text_color=8a919a&icon_color=6aa6f8&bg_color=0e1116" alt="terminal-site" />
+</a>
+
 <a href="https://github.com/paulasijit/coding_platform">
-  <img align="center" src="https://github-readme-stats.vercel.app/api/pin/?username=paulasijit&repo=coding_platform&show_icons=true&line_height=27&title_color=6aa6f8&text_color=8a919a&icon_color=6aa6f8&bg_color=0e1116" alt="DA-RNN" />
+  <img align="center" src="https://github-readme-stats.vercel.app/api/pin/?username=paulasijit&repo=coding_platform&show_icons=true&line_height=27&title_color=6aa6f8&text_color=8a919a&icon_color=6aa6f8&bg_color=0e1116" alt="coding_platform" />
 </a>
 
 <a href="https://github.com/paulasijit/python_keygen">
-  <img align="center" src="https://github-readme-stats.vercel.app/api/pin/?username=paulasijit&repo=python_keygen&show_icons=true&line_height=27&title_color=6aa6f8&text_color=8a919a&icon_color=6aa6f8&bg_color=0e1116" alt="crnn-pytorch" />
+  <img align="center" src="https://github-readme-stats.vercel.app/api/pin/?username=paulasijit&repo=python_keygen&show_icons=true&line_height=27&title_color=6aa6f8&text_color=8a919a&icon_color=6aa6f8&bg_color=0e1116" alt="python_keyge" />
 </a>
 
 ### 🧑‍💻  This week, I spent my time on:
